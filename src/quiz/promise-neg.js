1328bd41d17da274842be9fa53add7f42f46c01b
Vedant Rishi Das <55937481+vedant130701@users.noreply.github.com>
2024-02-27 17:02:43 -0500

    For you to do concurrency

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -1,5 +1,33 @@
+function processNeg1D(arr){
+    return new Promise((resolve, reject) => {
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                for (let i = 0; i < arr.length; ++i) {
+                    if(arr[i] < 0) {
+                        resolve(arr[i]);
+                    }
+                }
+            }, 0)
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+    })
+}
+
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+const neg0 = processNeg1D(array2D[0]);
+const neg1 = processNeg1D(array2D[1]);
+const neg2 = processNeg1D(array2D[2]);
+
+Promise.any([neg0, neg1, neg2])
+.then((responses) => {
+    console.log(`${responses}`);
+})
+.catch((error) => console.log(`${error}`));
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -1,5 +1,42 @@
+function sum1DArray(arr){
+    return new Promise((resolve, reject) => {
+        if(Array.isArray(arr)) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < arr.length; ++i) {
+                    sum+=arr[i];
+                }
+                resolve(sum);
+            }, 0)
+        }
+        else {
+            console.log('rejecting ... ');
+            reject('BAD INPUT: Expected array as input');
+        }
+    })
+}
+
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+const sumPromise0 = sum1DArray(array2D[0]);
+const sumPromise1 = sum1DArray(array2D[1]);
+const sumPromise2 = sum1DArray(array2D[2]);
+// console.log(sumPromise1);
+
+// can do a for loop and pass index in the function
+
+
+
+Promise.all([sumPromise0, sumPromise1, sumPromise2])
+.then((responses) => {
+    let sum = 0;
+    responses.forEach(rowSum => {
+        sum+= rowSum;
+    })
+    console.log(`Sum = ${sum}`);
+})
+.catch((error) => console.log(`${error}`));
